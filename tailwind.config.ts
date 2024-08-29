695d04a4d690555b8d55d05dd856938a51b900d7
sorrel012 <wain1719@gmail.com>
2024-08-29 10:31:41 +0900

    style: tailwindCSS에 블로그 테마 색상 추가

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,33 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+    content: [
+        './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+        './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+        './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    ],
+    theme: {
+        extend: {
+            colors: {
+                customLightBlue: {
+                    100: 'rgba(110, 127, 145, 0.5)',
+                    200: '#6E7F91',
+                },
+                customDarkBlue: {
+                    100: '#394C60',
+                },
+                customBeige: {
+                    100: '#EFECE7',
+                },
+                customGray: {
+                    100: '#CDCAC5',
+                },
+                customBrown: {
+                    100: '#A38970',
+                },
+            },
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
 };
 export default config;
