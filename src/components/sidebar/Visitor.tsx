d16e14e9d49fac59d775f0af4a848609842ef708
sorrel012 <wain1719@gmail.com>
2024-08-29 15:54:17 +0900

    feat: 사이드바 방문자 마크업

---
+++ src/components/sidebar/Visitor.tsx
@@ -0,0 +1,26 @@
+import { MdOutlinePerson } from 'react-icons/md';
+
+const visitors = [
+    'Jay',
+    '하이',
+    '닉네임',
+    'ㅁㅁㅁㅁㅁㅁㅁasdfasdfasdfasddfasdfasdfasdfasdf',
+];
+
+export default function Visitor() {
+    return (
+        <section className="font-default mt-4 px-2">
+            <div className="mb-2 font-bold">오늘 다녀갔어요</div>
+            <ul className="shadow-custom rounded-md p-2 text-sm leading-5">
+                {visitors.map((visitor, index) => (
+                    <li key={index} className="flex items-center">
+                        <span>
+                            <MdOutlinePerson className="size-4" />
+                        </span>
+                        <span className="truncate">{visitor}</span>
+                    </li>
+                ))}
+            </ul>
+        </section>
+    );
+}
