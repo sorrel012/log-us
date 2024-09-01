3877a2456ba6cebf97042c925ecc84b9477c2c84
sorrel012 <wain1719@gmail.com>
2024-09-01 15:35:41 +0900

    style: 사이드바 레이아웃 수정

--- src/app/blog/layout.tsx
+++ src/app/blog/layout.tsx
@@ -8,7 +8,9 @@ export default function BlogLayout({
     return (
         <div className="flex">
             <Sidebar />
-            <div className="ml-[20%] w-[80%]">{children}</div>
+            <div className="ml-[20%] w-[80%] lg:ml-[16.6667%] lg:w-[83.3333%]">
+                {children}
+            </div>
         </div>
     );
 }
