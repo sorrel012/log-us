be57eb0a8ff657602879dfc3a0d02006fca4b82f
sorrel012 <wain1719@gmail.com>
2024-09-26 17:50:47 +0900

    feat: 알림 모달 창 마크업

--- src/app/blog/@modal/(.)notification/page.tsx
+++ src/app/blog/@modal/(.)notification/page.tsx
@@ -1,7 +1,40 @@
+'use client';
+
+import { useRouter } from 'next/navigation';
+
 export default function NotificationModalPage() {
+    const router = useRouter();
+    const onCloseClick = () => {
+        router.back();
+    };
+
     return (
-        <div className="absolute left-0 top-0 z-[99] flex h-full w-full justify-center bg-black bg-opacity-60">
-            모달~
+        <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-60">
+            <div className="h-[60vh] w-[40vw] max-w-screen-lg rounded-lg bg-white shadow-lg lg:h-[65vh] lg:w-[50vw]">
+                {/*/!* Modal Header *!/*/}
+                {/*<div className="flex justify-between border-b border-gray-300 p-4">*/}
+                {/*    <h2 className="text-lg font-semibold">구독</h2>*/}
+                {/*    <button onClick={onCloseClick}>*/}
+                {/*        <IoCloseOutline*/}
+                {/*            size={24}*/}
+                {/*            className="text-gray-500 hover:text-black"*/}
+                {/*        />*/}
+                {/*    </button>*/}
+                {/*</div>*/}
+
+                {/*/!* Modal Body *!/*/}
+                {/*<div className="p-6">/!* 여기에 내용이 들어갑니다 *!/</div>*/}
+
+                {/*/!* Modal Footer *!/*/}
+                {/*<div className="flex justify-end border-t border-gray-300 p-4">*/}
+                {/*    <button*/}
+                {/*        onClick={onCloseClick}*/}
+                {/*        className="rounded-md bg-gray-500 px-4 py-2 text-white"*/}
+                {/*    >*/}
+                {/*        닫기*/}
+                {/*    </button>*/}
+                {/*</div>*/}
+            </div>
         </div>
     );
 }
