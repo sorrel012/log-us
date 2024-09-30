cef7f12f540b6f1a4f63e270f05a56c3154168b7
sorrel012 <wain1719@gmail.com>
2024-09-30 17:52:50 +0900

    feat: 시리즈 card 마크업

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,7 +1,5 @@
 'use client';
 
-import { useFetch } from '@/hooks/useFetch';
-
 import SeriesGrid from '@/components/blog/series/SeriesGrid';
 
 export default function BlogMain() {
@@ -10,15 +8,5 @@ export default function BlogMain() {
             <h2 className="font-bold">시리즈</h2>
             <SeriesGrid />
         </section>
-    );
-    const { data, isLoading, isError, error } = useFetch('/series.json');
-
-    return (
-        <div>
-            {isLoading && <div>로딩 중~~</div>}
-            {!isLoading && !isError && data && JSON.stringify(data)}
-            {!isLoading && !isError && !data && <div>데이터가 없습니다.</div>}
-            {isError && <div>{error}</div>}
-        </div>
     );
 }
--- src/components/blog/series/SeriesCard.tsx
+++ src/components/blog/series/SeriesCard.tsx
@@ -1,3 +1,10 @@
-export default function SeriesCard() {
-    return <>hi</>;
+import Image from 'next/image';
+
+export default function SeriesCard({ seriesName, imgUrl }) {
+    return (
+        <article className="w-full border border-solid border-customLightBlue-100">
+            <Image src={imgUrl} alt={seriesName} width={200} height={200} />
+            <div>{seriesName}</div>
+        </article>
+    );
 }
