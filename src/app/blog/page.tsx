9f83bb105995400356dee22c8493e7c1cdf68740
sorrel012 <wain1719@gmail.com>
2024-09-30 17:28:01 +0900

    feat: fetch 훅 구현

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,3 +1,16 @@
+'use client';
+
+import { useFetch } from '@/hooks/useFetch';
+
 export default function BlogMain() {
-    return <div>블로그 화면</div>;
+    const { data, isLoading, isError, error } = useFetch('/series.json');
+
+    return (
+        <div>
+            {isLoading && <div>로딩 중~~</div>}
+            {!isLoading && !isError && data && JSON.stringify(data)}
+            {!isLoading && !isError && !data && <div>데이터가 없습니다.</div>}
+            {isError && <div>{error}</div>}
+        </div>
+    );
 }
