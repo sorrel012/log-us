974568c39127dbfc1c8c58fa3deef4684c15197d
sorrel012 <wain1719@gmail.com>
2024-10-04 17:51:47 +0900

    feat: 블로그 주소로 아이디 조회

--- src/app/[blogAddress]/(blog)/posts/[series]/page.tsx
+++ src/app/[blogAddress]/(blog)/posts/[series]/page.tsx
@@ -2,6 +2,8 @@
 
 import { useParams } from 'next/navigation';
 import { useFetch } from '@/hooks/useFetch';
+import Popup from '@/components/Popup';
+import { useEffect, useState } from 'react';
 
 export default function PostListPage() {
     const { blogAddress, series } = useParams();
@@ -10,10 +12,40 @@ export default function PostListPage() {
             ? +decodeURIComponent(series).split('=')[1]
             : 0;
 
-    const blogId = useFetch('/blog-id.json', {
+    const [showPopup, setShowPopup] = useState(false);
+    const [popupMessage, setPopupMessage] = useState('');
+
+    const {
+        data: blogId,
+        isError: isBlogIdError,
+        error: blogIdError,
+    } = useFetch('/blog-id.json', {
         params: { blogAddress: blogAddress },
         queryKey: ['posts', seriesId],
     });
 
-    return <div>seriesId: {seriesId}</div>;
+    const handleClosePopup = () => {
+        setShowPopup(false);
+    };
+
+    useEffect(() => {
+        if (isBlogIdError && !showPopup) {
+            setPopupMessage(
+                blogIdError ? blogIdError : '알 수 없는 오류가 발생했습니다.',
+            );
+            setShowPopup(true);
+        }
+    }, [isBlogIdError, blogIdError]);
+
+    return (
+        <>
+            <div>seriesId: {seriesId}</div>
+            <Popup
+                show={showPopup}
+                title="에러"
+                text={popupMessage}
+                onClose={handleClosePopup}
+            />
+        </>
+    );
 }
