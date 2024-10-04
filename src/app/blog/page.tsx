95ccbb9181e06ea9db3d5d15594eae074ea9cea6
sorrel012 <wain1719@gmail.com>
2024-10-04 16:29:36 +0900

    feat: customFetch 캐싱 기능 추가

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -3,12 +3,14 @@
 import { useFetch } from '@/hooks/useFetch';
 
 export default function BlogMain() {
-    const { data, isLoading, isError, error } = useFetch('/series.json');
+    const { data, isLoading, isError, error } = useFetch('/data.json', {
+        queryKey: ['series'],
+    });
 
     return (
         <div>
             {isLoading && <div>로딩 중~~</div>}
-            {!isLoading && !isError && data && JSON.stringify(data)}
+            {!isLoading && !isError && data && <div>{data.length}</div>}
             {!isLoading && !isError && !data && <div>데이터가 없습니다.</div>}
             {isError && <div>{error}</div>}
         </div>
