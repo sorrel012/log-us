63d6924f1f5aefd5b0cffe1b12b9b53e9b83a417
sorrel012 <wain1719@gmail.com>
2024-10-04 09:41:32 +0900

    feat: url 제외 기타 옵션 config 처리

--- src/utils/customFetch.ts
+++ src/utils/customFetch.ts
@@ -1,35 +1,54 @@
 const TIMEOUT = 30 * 1000;
 
+interface FetchState<T> {
+    data?: T;
+    isLoading: boolean;
+    isError: boolean;
+    error?: string;
+}
+
+export interface FetchConfig {
+    method?: HttpMethod;
+    data?: any;
+    headers?: Record<string, string>;
+    options?: any;
+}
+
+type HttpMethod = 'GET' | 'POST' | 'PUT' | 'DELETE';
+
 export async function customFetch<T>(
     url: string,
-    method: 'GET' | 'POST' | 'PUT' | 'DELETE' = 'GET',
-    body?: any,
-    headers?: Record<string, string>,
-    options?: Omit<RequestInit, 'headers'>,
-): Promise<T> {
+    config: FetchConfig = {},
+): Promise<FetchState<T>> {
     const controller = new AbortController();
     const { signal } = controller;
 
-    if (!headers) {
-        headers = {};
+    let { method, data, headers = {}, options = {} } = config;
+
+    if (!method) {
+        method = 'GET';
     }
 
     if (!headers['Content-Type']) {
         headers['Content-Type'] = 'application/json';
     }
-
-    const baseUrl = process.env.NEXT_PUBLIC_API_URL;
-
     //TODO mber-id zustand에서 받아오는 걸로 수정 필요
     headers['member-id'] = 'sorrel012';
 
+    let baseUrl = process.env.NEXT_PUBLIC_API_URL;
+
     const timeoutId = setTimeout(() => controller.abort(), TIMEOUT);
+
+    let state: FetchState<T> = {
+        isLoading: true,
+        isError: false,
+    };
 
     try {
         const response = await fetch(`${baseUrl}${url}`, {
-            method: method,
+            method,
+            ...(data && { body: JSON.stringify(data) }),
             headers,
-            ...(body ? { body: JSON.stringify(body) } : {}),
             ...options,
             signal,
         });
@@ -37,22 +56,26 @@ export async function customFetch<T>(
         clearTimeout(timeoutId);
 
         if (!response.ok) {
-            throw new Error(
-                `서버 오류가 발생하였습니다. ${response.statusText}`,
-            );
+            state.isError = true;
+            state.error = `서버 오류가 발생하였습니다. ${response.statusText}`;
         }
 
-        // JSON 데이터를 반환
-        return (await response.json()) as T;
+        state.data = (await response.json()) as T;
+        state.isLoading = false;
     } catch (error) {
         clearTimeout(timeoutId);
+        state.isLoading = false;
+        state.isError = true;
 
         if (error.name === 'AbortError') {
-            throw new Error(
-                '응답 시간이 지연되고 있습니다. 잠시 후 다시 시도해 주세요.',
-            );
+            state.error =
+                '응답 시간이 지연되고 있습니다. 잠시 후 다시 시도해 주세요.';
+        } else {
+            state.error = error.message || '네트워크 오류가 발생했습니다.';
         }
-
-        throw new Error(error.message || '네트워크 오류가 발생했습니다.');
+    } finally {
+        clearTimeout(timeoutId);
     }
+
+    return state;
 }
