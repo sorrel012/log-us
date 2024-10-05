a7173a8f6bb7c9a181c902e659bdfebe26840db0
leeeeeeeminji <ldfu0269@naver.com>
2024-10-06 01:38:44 +0900

    style : 검색 컴포넌트 framer-motion 적용

--- src/components/search/Search.tsx
+++ src/components/search/Search.tsx
@@ -1,5 +1,6 @@
 import { IoSearch } from 'react-icons/io5';
 import { useState } from 'react';
+import { motion } from 'framer-motion';
 
 export default function Search() {
     const [activeSearch, setActiveSearch] = useState(false);
@@ -8,22 +9,25 @@ export default function Search() {
         setActiveSearch(!activeSearch);
     };
 
+
     return (
         <div className='relative'>
             {activeSearch && (
-                <div className='relative w-64'>
-                    <input type='text'
-                        className='w-full px-4 py-2 pr-10 text-md rounded-md outline-0 border-2 border-customLightBlue-100 text-gray-500 focus:border-customLightBlue-200'
-                        placeholder='검색어를 입력해주세요.'
-                        autoFocus
-                    />
-                    <button
-                        onClick={ClickSearchBtn}
-                        className='absolute top-1/2 right-2 transform -translate-y-1/2'
-                    >
-                        <IoSearch className='text-2xl text-customLightBlue-200' />
-                    </button>
-                </div>
+                <motion.div animate={{width: activeSearch ? '16rem' : '2.5rem', opacity : activeSearch ? 1 : 0.5}} transition={{duration:0.4}}>
+                    <div className='relative'>
+                        <input type='text'
+                            className='w-full px-4 py-2 pr-10 text-md rounded-md outline-0 border-2 border-customLightBlue-100 text-gray-500 focus:border-customLightBlue-200'
+                            placeholder='검색어를 입력해주세요.'
+                            autoFocus
+                        />
+                        <button
+                            onClick={ClickSearchBtn}
+                            className='absolute top-1/2 right-2 transform -translate-y-1/2'
+                        >
+                            <IoSearch className='text-2xl text-customLightBlue-200' />
+                        </button>
+                    </div>
+                </motion.div>
             )}
 
             {!activeSearch && (
