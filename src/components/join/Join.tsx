edcd6ee47cf6c85f03bba668bdb0e084536f2942
leeeeeeeminji <ldfu0269@naver.com>
2024-10-06 01:51:23 +0900

    style : div 간격 조정

--- src/components/join/Join.tsx
+++ src/components/join/Join.tsx
@@ -5,16 +5,16 @@ export default function Join() {
 
             <div className='bg-customGray-200 w-5/12 mx-auto my-0 rounded-lg px-10 py-10 h-auto'>
                 {/* 내 정보 입력 */}
-                <div className='text-lg font-semibold border-b-2 border-solid border-gray-200 pb-2'>내 정보 입력</div>
+                <div className='text-lg font-semibold border-b-2 border-solid border-gray-200 pb-4'>내 정보 입력</div>
                 <div className='p-5'>
-                    <div className='flex items-center mb-5'>
+                    <div className='flex items-center mb-10'>
                         <div className='font-semibold w-2/12'>아이디<span className='ml-1 text-red-400'>*</span></div>
                         <div className='w-full flex'>
                             <input type='text' className='w-10/12 h-8 rounded-sm pl-2 ml-4 outline-none' placeholder='영어 소문자, 숫자, 특수기호(-), (_)로 5~20자 이내'/>
                             <button className='w-3/12 bg-customGray-100/60 rounded-e-sm hover:bg-customGray-100 transition-colors duration-300' >중복 확인</button>
                         </div>
                     </div>
-                    <div className='flex items-center mb-5'>
+                    <div className='flex items-center mb-8'>
                         <div className='font-semibold w-2/12'>닉네임<span className='ml-1 text-red-400'>*</span></div>
                         <input type='text' className='w-10/12 h-8 rounded-sm pl-2 outline-none'/>
                     </div>
@@ -31,22 +31,22 @@ export default function Join() {
                             </div>
                         </div>
                     </div>
-                    <div className='flex items-center mb-5'>
+                    <div className='flex items-center mb-8'>
                         <div className='font-semibold w-2/12'>비밀번호<span className='ml-1 text-red-400'>*</span></div>
                         <input type='password' className='w-10/12 h-8 rounded-sm pl-2 outline-none' placeholder='영문 대/소문자, 숫자, 특수문자 중 2가지 혼합하여 8~16자 이내'/>
                     </div>
-                    <div className='flex items-center mb-5'>
+                    <div className='flex items-center mb-8'>
                         <div className='font-semibold w-2/12'>비밀번호 확인<span className='ml-1 text-red-400'>*</span></div>
                         <input type='password' className='w-10/12 h-8 rounded-sm pl-2 outline-none' placeholder='영문 대/소문자, 숫자, 특수문자 중 2가지 혼합하여 8~16자 이내'/>
                     </div>
-                    <div className='flex items-center mb-5'>
+                    <div className='flex items-center mb-8'>
                         <div className='font-semibold w-2/12'>이메일<span className='ml-1 text-red-400'>*</span></div>
                         <div className='w-full flex'>
                             <input type='text' className='w-6/12 h-8 rounded-sm pl-2 ml-4 outline-none' />
                             <button className='w-2/12 bg-zinc-200/80 rounded-sm ml-3 hover:bg-zinc-300 transition-colors duration-300'>인증 코드 발송</button>
                         </div>
                     </div>
-                    <div className='flex items-center mb-5'>
+                    <div className='flex items-center mb-8'>
                         <div className='font-semibold w-2/12'>인증 코드<span className='ml-1 text-red-400'>*</span></div>
                         <div className='w-full flex'>
                             <input type='text' className='w-6/12 h-8 rounded-sm pl-2 ml-4 outline-none' />
@@ -56,13 +56,13 @@ export default function Join() {
                 </div>
                 
                 {/* 블로그 정보 입력 */}
-                <div className='text-lg font-semibold border-b-2 border-solid border-gray-200 pb-2 mt-3'>블로그 정보 입력</div>
+                <div className='text-lg font-semibold border-b-2 border-solid border-gray-200 pb-4 mt-3'>블로그 정보 입력</div>
                 <div className='p-5'>
-                    <div className='flex items-center mb-5'>
+                    <div className='flex items-center mb-8'>
                         <div className='font-semibold w-2/12'>블로그명<span className='ml-1 text-red-400'>*</span></div>
                         <input type='text' className='w-10/12 h-8 rounded-sm pl-2 outline-none'/>
                     </div>
-                    <div className='flex items-center mb-5'>
+                    <div className='flex items-center mb-8'>
                         <div className='font-semibold w-2/12'>블로그 주소<span className='ml-1 text-red-400'>*</span></div>
                         <div className='w-full flex'>
                             <input type='text' className='w-4/12 h-8 rounded-sm pl-2 ml-4 outline-none'/>
