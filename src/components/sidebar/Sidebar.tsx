ac8c2a8c37aba0373656269489d60bc17c179290
sorrel012 <wain1719@gmail.com>
2024-10-09 00:16:05 +0900

    feat: 사이드바 로딩스피너 구현

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -3,9 +3,11 @@ import Visitor from '@/components/sidebar/Visitor';
 import PanelModule from '@/components/sidebar/PanelModule';
 import { UseSeries } from '@/hooks/useSeries';
 import Popup from '@/components/Popup';
+import LoadingSpinner from '@/components/ui/LoadingSpinner';
 
 export default function Sidebar() {
-    const { data, showPopup, popupMessage, handleClosePopup } = UseSeries();
+    const { data, isLoading, showPopup, popupMessage, handleClosePopup } =
+        UseSeries();
 
     const series = data
         ? [
@@ -23,8 +25,14 @@ export default function Sidebar() {
         <>
             <aside className="fixed flex h-[100vh] w-1/5 flex-col overflow-y-auto border-r border-solid border-customLightBlue-100 lg:w-1/6">
                 <div className="flex-1 p-5">
+                    <div>{`isLoading: ${isLoading}`}</div>
                     <UserProfile />
                     {series && <PanelModule title="시리즈" contents={series} />}
+                    {isLoading && (
+                        <div className="mt-3 px-2">
+                            <LoadingSpinner />
+                        </div>
+                    )}
                 </div>
                 <div className="mb-14 p-5">
                     <Visitor />
