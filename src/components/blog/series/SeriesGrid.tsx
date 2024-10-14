c1ed44472fa39aaf154b427865224de9fdb4005b
sorrel012 <wain1719@gmail.com>
2024-10-14 14:00:36 +0900

    refactor: 데이터 fetching 결과 분기 처리 리팩토링

--- src/components/blog/series/SeriesGrid.tsx
+++ src/components/blog/series/SeriesGrid.tsx
@@ -15,14 +15,20 @@ export default function SeriesGrid() {
 
     return (
         <section>
-            {isLoading && (
+            {isLoading ? (
                 <div className="grid grid-cols-1 gap-10 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-7">
                     {Array.from({ length: 9 }).map((_, index) => (
                         <SeriesSkeleton key={index} />
                     ))}
                 </div>
-            )}
-            {!isLoading && !isError && series && (
+            ) : isError ? (
+                <div className="leading-6">
+                    <div>시리즈를 불러올 수 없습니다.</div>
+                    <div>잠시 후 다시 시도해주세요.</div>
+                </div>
+            ) : series?.length === 0 ? (
+                <div>시리즈가 존재하지 않습니다.</div>
+            ) : (
                 <div className="grid grid-cols-1 gap-10 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-7">
                     {series.map((item) => (
                         <SeriesCard
@@ -33,15 +39,6 @@ export default function SeriesGrid() {
                         />
                     ))}
                 </div>
-            )}
-            {!isLoading && isError && (
-                <div className="leading-6">
-                    <div>시리즈를 불러올 수 없습니다. </div>
-                    <div>잠시 후 다시 시도해주세요. </div>
-                </div>
-            )}
-            {!isLoading && !isError && series.length === 0 && (
-                <div>시리즈가 존재하지 않습니다.</div>
             )}
 
             <Popup
