114a8ba64c8921e26156e9494085e77dc17f8034
sorrel012 <wain1719@gmail.com>
2024-10-15 22:21:37 +0900

    style: 팝업 애니메이션 추가

--- src/components/Popup.tsx
+++ src/components/Popup.tsx
@@ -1,4 +1,6 @@
+import { useEffect, useState } from 'react';
 import { createPortal } from 'react-dom';
+import { AnimatePresence, motion } from 'framer-motion';
 
 interface PopupProps {
     show: boolean;
@@ -9,6 +11,26 @@ interface PopupProps {
     type?: string;
 }
 
+const popupVariants = {
+    hidden: {
+        opacity: 0,
+    },
+    visible: {
+        opacity: 1,
+        transition: {
+            duration: 0.15,
+            ease: 'easeOut',
+        },
+    },
+    exit: {
+        opacity: 0,
+        transition: {
+            duration: 0.15,
+            ease: 'easeOut',
+        },
+    },
+};
+
 export default function Popup({
     show,
     title,
@@ -17,27 +39,55 @@ export default function Popup({
     onClose,
     type = 'alert',
 }: PopupProps) {
-    if (!show) return null;
+    const [isClient, setIsClient] = useState(false);
+    const [modalContainer, setModalContainer] = useState<HTMLElement | null>(
+        null,
+    );
+
+    useEffect(() => {
+        setIsClient(true);
+        setModalContainer(document.getElementById('modal'));
+    }, []);
+
+    if (!isClient || !modalContainer) {
+        return null;
+    }
 
     return createPortal(
-        <div className="popup">
-            <div className="container">
-                <div>
-                    <h2>{title}</h2>
-                    {text && <p>{text}</p>}
-                </div>
-                <div className="button">
-                    {type === 'confirm' && (
-                        <button className="confirm" onClick={onConfirm}>
-                            취소
-                        </button>
-                    )}
-                    <button className="close" onClick={onClose}>
-                        확인
-                    </button>
-                </div>
-            </div>
-        </div>,
-        document.getElementById('modal'),
+        <AnimatePresence>
+            {show && (
+                <motion.div
+                    key="container"
+                    className="popup"
+                    exit="exit"
+                    variants={popupVariants}
+                >
+                    <motion.div
+                        key="popup"
+                        className="container"
+                        initial="hidden"
+                        animate="visible"
+                        exit="exit"
+                        variants={popupVariants}
+                    >
+                        <div>
+                            <h2>{title}</h2>
+                            {text && <p>{text}</p>}
+                        </div>
+                        <div className="button">
+                            {type === 'confirm' && (
+                                <button className="confirm" onClick={onConfirm}>
+                                    취소
+                                </button>
+                            )}
+                            <button className="close" onClick={onClose}>
+                                확인
+                            </button>
+                        </div>
+                    </motion.div>
+                </motion.div>
+            )}
+        </AnimatePresence>,
+        modalContainer,
     );
 }
