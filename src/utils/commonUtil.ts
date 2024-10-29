1ebe114dca7879b30300a7f5e468392cbb7c54fe
sorrel012 <wain1719@gmail.com>
2024-10-29 16:07:14 +0900

    feat: 객체 비교 공통함수 구현

--- src/utils/commonUtil.ts
+++ src/utils/commonUtil.ts
@@ -8,3 +8,32 @@ export function dateFormatter(date: Date) {
         hour12: false,
     });
 }
+
+export function isObjEqual(
+    obj1: Record<string, any>,
+    obj2: Record<string, any>,
+): boolean {
+    const keys1 = Object.keys(obj1);
+    const keys2 = Object.keys(obj2);
+
+    if (keys1.length !== keys2.length) return false;
+
+    for (const key of keys1) {
+        const val1 = obj1[key];
+        const val2 = obj2[key];
+
+        const areObjects = isObject(val1) && isObject(val2);
+        if (
+            (areObjects && !isEqual(val1, val2)) ||
+            (!areObjects && val1 !== val2)
+        ) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+function isObject(object: any): boolean {
+    return object != null && typeof object === 'object';
+}
