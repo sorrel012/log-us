51186f5a1de34606e4d28cdafef1549482f6203c
sorrel012 <wain1719@gmail.com>
2024-11-15 13:58:43 +0900

    fix: 텍스트 에디터 내 커서 이동 수정

--- src/components/blog/TextEditor.tsx
+++ src/components/blog/TextEditor.tsx
@@ -1,13 +1,10 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useRef } from 'react';
 import '@toast-ui/editor/dist/toastui-editor.css';
 import 'tui-color-picker/dist/tui-color-picker.css';
 import '@toast-ui/editor-plugin-color-syntax/dist/toastui-editor-plugin-color-syntax.css';
 import '@toast-ui/editor/dist/i18n/ko-kr';
 import { Editor } from '@toast-ui/react-editor';
 import colorSyntax from '@toast-ui/editor-plugin-color-syntax';
-import { HookCallback } from '@toast-ui/editor/types/editor';
-import Popup from '@/components/Popup';
-import { customFetch } from '@/utils/customFetch';
 
 const toolbarItems = [
     ['heading', 'bold', 'italic', 'strike'],
@@ -29,43 +26,16 @@ export default function TextEditor({
     const editorRef = useRef<Editor>();
 
     useEffect(() => {
-        editorRef.current?.getInstance().setHTML(contents || '');
+        if (contents && !editorRef.current?.getInstance().getHTML()) {
+            editorRef.current?.getInstance().setHTML(contents);
+        }
     }, [contents]);
 
     useEffect(() => {
-        editorRef.current?.getInstance().focus();
-    }, [isEmpty]);
-
-    const handleImageUpload = async (
-        image: File | Blob,
-        callback: HookCallback,
-    ) => {
-        const formData = new FormData();
-        formData.append('file', image);
-
-        try {
-            const response = await customFetch('/temporary-image', {
-                method: 'POST',
-                body: formData,
-                queryKey: ['imageUpload'],
-            });
-
-            const imageUrl = response.data?.filepath;
-            const imageName = response.data?.orgFilename;
-
-            if (imageUrl) {
-                callback(imageUrl, imageName);
-            }
-        } catch (error) {
-            setShowPopup(true);
+        if (isEmpty) {
+            editorRef.current?.getInstance().focus();
         }
-    };
-
-    const [showPopup, setShowPopup] = useState(false);
-
-    const handleConfirm = () => {
-        setShowPopup(false);
-    };
+    }, [isEmpty]);
 
     const handleChange = () => {
         const content = editorRef.current?.getInstance().getHTML();
@@ -75,26 +45,17 @@ export default function TextEditor({
     };
 
     return (
-        <>
-            <Editor
-                ref={editorRef}
-                height="80vh"
-                initialValue={contents}
-                placeholder="내용을 입력하세요"
-                initialEditType="wysiwyg"
-                hideModeSwitch={true}
-                plugins={[colorSyntax]}
-                toolbarItems={toolbarItems}
-                hooks={{ addImageBlobHook: handleImageUpload }}
-                language="ko-KR"
-                onChange={handleChange}
-            />
-            <Popup
-                show={showPopup}
-                text="이미지 업로드에 실패했습니다. 다시 시도해주세요."
-                type="alert"
-                onConfirm={handleConfirm}
-            />
-        </>
+        <Editor
+            ref={editorRef}
+            height="80vh"
+            initialValue=""
+            placeholder="내용을 입력하세요"
+            initialEditType="wysiwyg"
+            hideModeSwitch={true}
+            plugins={[colorSyntax]}
+            toolbarItems={toolbarItems}
+            language="ko-KR"
+            onChange={handleChange}
+        />
     );
 }
