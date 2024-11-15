6f926ffaab1d270fc88a0a035104cd4c816b9bee
sorrel012 <wain1719@gmail.com>
2024-11-15 14:02:41 +0900

    fix: 게시글 제목, 본문 이스케이프 처리 수정

--- src/app/[blogAddress]/(blog)/newpost/page.tsx
+++ src/app/[blogAddress]/(blog)/newpost/page.tsx
@@ -263,7 +263,8 @@ export default function NewPostPage() {
 
             return;
         }
-        if (content.length > 10000) {
+
+        if (content.length > 5000) {
             setPopupId('CONTENT_FOCUS');
             setPopupTitle('내용을 10,000자 이내로 작성해 주세요.');
             setShowPopup(true);
