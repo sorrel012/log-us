7de19ab68ff319b2266ee9dddcdf0855d128c177
sorrel012 <wain1719@gmail.com>
2024-11-25 17:58:39 +0900

    feat: 블로그 삭제 마크업

--- src/app/[blogAddress]/setting/info/page.tsx
+++ src/app/[blogAddress]/setting/info/page.tsx
@@ -23,6 +23,7 @@ export default function BlogInfo() {
     const [blogAddressMessage, setBlogAddressMessage] = useState(
         '사용할 수 없는 블로그 주소입니다.',
     );
+    const [wantDelete, setWantDelete] = useState<boolean>(false);
 
     useEffect(() => {
         if (blogInfo) {
@@ -179,6 +180,8 @@ export default function BlogInfo() {
         }
     };
 
+    const handleDeleteBlog = () => {};
+
     return (
         <section>
             <h2 className="mb-8 text-lg font-bold">블로그 정보 변경</h2>
@@ -252,6 +255,35 @@ export default function BlogInfo() {
                         onClick={handleSaveBlogInfo}
                     >
                         저장
+                    </button>
+                </div>
+            </div>
+            <div>
+                <h2 className="mb-4 text-lg font-bold">블로그 삭제</h2>
+                <div className="text-md leading-6">
+                    <p>블로그를 삭제할 경우 블로그의 모든 내용이 삭제됩니다.</p>
+                    <p>삭제된 블로그는 다시 복구할 수 없습니다.</p>
+                    <p>동의할 경우 삭제 버튼을 눌러 블로그를 삭제해 주세요</p>
+                </div>
+                <div className="mt-2 flex items-center text-sm text-customLightBlue-200">
+                    <input
+                        type="checkbox"
+                        checked={wantDelete || false}
+                        onChange={(e) => setWantDelete(e.target.checked)}
+                        className="scale-75"
+                        id="deleteBlog"
+                    />
+                    <label htmlFor="deleteBlog" className="ml-1 cursor-pointer">
+                        유의사항을 모두 확인하였으며, 삭제를 희망합니다.
+                    </label>
+                </div>
+                <div className="-mt-2 text-right">
+                    <button
+                        className={`rounded px-4 py-2 text-md ${wantDelete ? 'border border-customBrown-100 text-customBrown-100' : 'bg-neutral-300 text-neutral-700'}`}
+                        disabled={!wantDelete}
+                        onClick={handleDeleteBlog}
+                    >
+                        삭제
                     </button>
                 </div>
             </div>
