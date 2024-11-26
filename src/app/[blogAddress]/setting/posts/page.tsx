230480bec4b7e24a1ab2794274323a5029b19b17
sorrel012 <wain1719@gmail.com>
2024-11-26 16:12:08 +0900

    feat: 글 관리 임시 마크업

---
+++ src/app/[blogAddress]/setting/posts/page.tsx
@@ -0,0 +1,22 @@
+'use client';
+
+import { useState } from 'react';
+
+export default function PostsManagePage() {
+    const [totalPosts, setTotalPosts] = useState(0);
+
+    return (
+        <filedset>
+            <div>
+                <legend className="mb-8 text-lg font-bold">글 관리</legend>
+                <span
+                    className="relative ml-0.5 text-sm text-customLightBlue-200"
+                    style={{ top: '-0.1em' }}
+                >
+                    {totalPosts}
+                </span>
+            </div>
+            {/*<ContentsSettingList contents={} buttons={} onChange={} />*/}
+        </filedset>
+    );
+}
