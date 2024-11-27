cef9cf3f436d0bcc1a7eb633612981650b0a9429
sorrel012 <wain1719@gmail.com>
2024-11-27 16:45:05 +0900

    feat: 시리즈 목록 관리 구현

--- src/components/blog/setting/SeriesList.tsx
+++ src/components/blog/setting/SeriesList.tsx
@@ -1,48 +1,185 @@
 'use client';
 
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { UseSeries } from '@/hooks/useSeries';
+import { BsList } from 'react-icons/bs';
+import { DragDropContext, Draggable, Droppable } from 'react-beautiful-dnd';
+import { SeriesGridProps } from '@/components/blog/series/SeriesCard';
+import AlertPopup from '@/components/AlertPopup';
+import { customFetch } from '@/utils/customFetch';
+import { useRouter } from 'next/navigation';
 
-export default function SeriesList() {
+export default function SeriesList({
+    onEdit,
+    onAdd,
+}: {
+    onEdit: (seriesId: number) => void;
+    onAdd: (isAddMode: boolean) => void;
+}) {
+    const router = useRouter();
     const { data } = UseSeries();
-
+    const [seriesList, setSeriesList] = useState<SeriesGridProps[]>(data);
     const [clickedSeriesId, setClickedSeriesId] = useState(0);
+    const [isAddMode, setIsAddMode] = useState(false);
 
-    console.log('음?', data);
+    const [showPopup, setShowPopup] = useState(false);
+    const [popupTitle, setPopupTitle] = useState('');
+    const [popupText, setPopupText] = useState('');
+
+    useEffect(() => {
+        if (data) {
+            setSeriesList(data);
+        }
+    }, [data]);
+
+    const handleDragEnd = async (result: any) => {
+        if (!result.destination) return;
+
+        const newSeriesList = Array.from(seriesList);
+        const [removed] = newSeriesList.splice(result.source.index, 1);
+        newSeriesList.splice(result.destination.index, 0, removed);
+        const reorderedSeriesList = newSeriesList.map((series, index) => ({
+            ...series,
+            seriesOrder: index + 1,
+        }));
+
+        setSeriesList(reorderedSeriesList);
+        const body = reorderedSeriesList.map((series) => ({
+            seriesId: series.seriesId,
+            seriesOrder: series.seriesOrder,
+        }));
+
+        const res = await customFetch('/series/order', {
+            method: 'POST',
+            queryKey: ['series', 'order'],
+            body: { seriesList: body },
+        });
+
+        if (res.isError) {
+            setPopupTitle('시리즈 순서를 저장하는 중 오류가<br> 발생했습니다.');
+            setPopupText('잠시 후 다시 시도해 주세요.');
+            setShowPopup(true);
+            return;
+        }
+    };
+
+    const handleClick = (seriesId: number) => {
+        onEdit(0);
+        setIsAddMode(false);
+
+        setClickedSeriesId((prevState) =>
+            prevState === seriesId ? 0 : seriesId,
+        );
+        onEdit(seriesId);
+    };
+
+    const handleAdd = () => {
+        setClickedSeriesId(0);
+        onAdd(!isAddMode);
+        setIsAddMode((prevState) => !prevState);
+    };
+
+    const handleDelete = async (e, seriesId: number) => {
+        e.stopPropagation();
+        const res = await customFetch('/series/' + seriesId, {
+            method: 'DELETE',
+            queryKey: ['series', 'delete', seriesId],
+        });
+
+        if (res.isError) {
+            setPopupTitle('시리즈를 삭제하는 중 오류가<br> 발생했습니다.');
+            setPopupText('잠시 후 다시 시도해 주세요.');
+            setShowPopup(true);
+            return;
+        }
+
+        setPopupTitle('시리즈가 삭제되었습니다.');
+        setPopupText('');
+        setShowPopup(true);
+        router.refresh();
+    };
 
     return (
-        <article className="rounded bg-customBeige-100 p-4">
-            <ul className="space-y-2">
-                {data &&
-                    data.map(({ seriesId, seriesName, imgUrl }) => (
-                        <li
-                            key={seriesId}
-                            className={`${clickedSeriesId === seriesId ? 'bg-customBrown-100 text-white' : 'bg-white text-customBrown-100'} flex cursor-pointer items-center justify-between rounded border border-solid border-customBrown-100 px-3 py-1.5`}
-                            onClick={() =>
-                                setClickedSeriesId((prevState) =>
-                                    prevState === seriesId ? 0 : seriesId,
-                                )
-                            }
-                        >
-                            <span className="text-customBeige-800 py-1 text-md">
-                                {seriesName}
-                            </span>
-                            <div
-                                className={`${clickedSeriesId !== seriesId && 'hidden'} select-none`}
-                            >
-                                <button className="mr-2 rounded bg-white px-2 py-0.5 text-sm text-customBrown-100">
-                                    수정
-                                </button>
-                                <button className="rounded bg-white px-2 py-0.5 text-sm text-customBrown-100">
-                                    삭제
-                                </button>
-                            </div>
-                        </li>
-                    ))}
-            </ul>
-            <button className="mt-4 w-full rounded border border-customBrown-100 py-2 text-customBrown-100">
+        <>
+            <button
+                className={`${isAddMode ? 'bg-customBrown-100 text-white' : 'bg-white text-customBrown-100'} w-full rounded border border-customBrown-100 py-2`}
+                onClick={handleAdd}
+            >
                 + 시리즈 추가
             </button>
-        </article>
+            <DragDropContext onDragEnd={handleDragEnd}>
+                <Droppable droppableId="seriesList">
+                    {(provided) => (
+                        <ul
+                            className="mt-10 space-y-2"
+                            {...provided.droppableProps}
+                            ref={provided.innerRef}
+                        >
+                            {seriesList &&
+                                seriesList.map(
+                                    (
+                                        { seriesId, seriesName, imgUrl },
+                                        index,
+                                    ) => (
+                                        <Draggable
+                                            key={seriesId}
+                                            draggableId={seriesId + ''}
+                                            index={index}
+                                        >
+                                            {(provided) => (
+                                                <li
+                                                    ref={provided.innerRef}
+                                                    {...provided.draggableProps}
+                                                    {...provided.dragHandleProps}
+                                                    className={`${clickedSeriesId === seriesId ? 'bg-customBrown-100 text-white' : 'bg-white text-customBrown-100'} flex cursor-pointer items-center justify-between rounded border border-solid border-customBrown-100 px-3 py-1.5`}
+                                                    style={{
+                                                        ...provided
+                                                            .draggableProps
+                                                            .style,
+                                                        userSelect: 'none',
+                                                    }}
+                                                    onClick={() => {
+                                                        handleClick(seriesId);
+                                                    }}
+                                                >
+                                                    <div className="flex items-center gap-2">
+                                                        <BsList />
+                                                        <span className="text-customBeige-800 py-1 text-md">
+                                                            {seriesName}
+                                                        </span>
+                                                    </div>
+                                                    <div
+                                                        className={`${clickedSeriesId !== seriesId && 'hidden'} select-none`}
+                                                    >
+                                                        <button
+                                                            className="rounded bg-white px-2 py-0.5 text-sm text-customBrown-100"
+                                                            onClick={(e) =>
+                                                                handleDelete(
+                                                                    e,
+                                                                    seriesId,
+                                                                )
+                                                            }
+                                                        >
+                                                            삭제
+                                                        </button>
+                                                    </div>
+                                                </li>
+                                            )}
+                                        </Draggable>
+                                    ),
+                                )}
+                            {provided.placeholder}
+                        </ul>
+                    )}
+                </Droppable>
+            </DragDropContext>
+
+            <AlertPopup
+                show={showPopup}
+                onConfirm={() => setShowPopup(false)}
+                title={popupTitle}
+                text={popupText}
+            />
+        </>
     );
 }
