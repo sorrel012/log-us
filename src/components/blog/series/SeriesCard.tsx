b038ec18be5ca4c56130e703d9b38f0876b4e78a
sorrel012 <wain1719@gmail.com>
2024-11-28 18:01:11 +0900

    fix: 시리즈 이미지 없는 경우 처리

--- src/components/blog/series/SeriesCard.tsx
+++ src/components/blog/series/SeriesCard.tsx
@@ -2,6 +2,8 @@
 
 import Image from 'next/image';
 import { useParams, useRouter } from 'next/navigation';
+import React from 'react';
+import { HiPhoto } from 'react-icons/hi2';
 
 export interface SeriesGridProps {
     seriesId: number;
@@ -27,14 +29,20 @@ export default function SeriesCard({
             className="flex aspect-square cursor-pointer flex-col items-center justify-center rounded-xl border border-solid border-customLightBlue-100 p-5 text-center"
             onClick={handleClick}
         >
-            <Image
-                src={imgUrl}
-                alt={seriesName}
-                className="aspect-square rounded-md object-cover"
-                priority
-                width={150}
-                height={150}
-            />
+            {imgUrl ? (
+                <Image
+                    src={imgUrl}
+                    alt={seriesName}
+                    className="aspect-square rounded-md object-cover"
+                    priority
+                    width={150}
+                    height={150}
+                />
+            ) : (
+                <div className="flex h-[150px] w-[150px] justify-center">
+                    <HiPhoto className="aspect-square size-full text-customLightBlue-100" />
+                </div>
+            )}
             <div className="mt-4 font-bold">{seriesName}</div>
         </article>
     );
