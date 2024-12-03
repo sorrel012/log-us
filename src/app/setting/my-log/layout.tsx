212f2760d39e858b61446d2bc0d94ffdaef7e298
sorrel012 <wain1719@gmail.com>
2024-12-03 10:53:02 +0900

    feat: 기본 블로그 정보 세팅 구현

--- src/app/setting/my-log/layout.tsx
+++ src/app/setting/my-log/layout.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useBlogStore } from '@/store/useBlogStore';
+import { BlogInfo, useBlogStore } from '@/store/useBlogStore';
 import { useEffect } from 'react';
 import { customFetch } from '@/utils/customFetch';
 
@@ -9,16 +9,26 @@ export default function MyLogSettingLayout({
 }: Readonly<{
     children: React.ReactNode;
 }>) {
-    const { setBlogId } = useBlogStore();
+    const { setBlogId, setBlogInfo } = useBlogStore();
 
     useEffect(() => {
         (async () => {
             const res = await customFetch('/blog/my-log', {
                 queryKey: ['my-log'],
             });
-            setBlogId(res.data.blogId);
+
+            if (res.data.blogId) {
+                setBlogId(res.data.blogId);
+
+                const blogInfoRes = await customFetch<BlogInfo>('/blog-info', {
+                    queryKey: ['memberInfo'],
+                    params: { blogId: res.data.blogId },
+                });
+
+                setBlogInfo(blogInfoRes.data!);
+            }
         })();
-    }, [setBlogId]);
+    }, [setBlogId, setBlogInfo]);
 
     return <>{children}</>;
 }
