44e6c49a55f0f61d3247915c392ca691d8b5f807
sorrel012 <wain1719@gmail.com>
2024-12-26 17:11:02 +0900

    fix: 문자열 quote 수정

--- src/components/@Modal/JoinModal.tsx
+++ src/components/@Modal/JoinModal.tsx
@@ -13,37 +13,64 @@ interface JoinModalProps {
 const JoinModal: React.FC<JoinModalProps> = ({ isOpen, closeModal }) => {
     if (!isOpen) {
         return null;
-    };
+    }
 
     return (
-        <div className='modal-backdrop' onClick={closeModal}>
-            <motion.div initial={{ y: -100, opacity: 0 }} animate={{ y: 0, opacity: 1 }} transition={{ duration: 0.5, ease: "easeOut" }} exit={{ y: -100, opacity: 0 }}>
-                <div className='modal-content' onClick={(e) => e.stopPropagation()}>
-                    <button className='close-btn' onClick={closeModal}>
+        <div className="modal-backdrop" onClick={closeModal}>
+            <motion.div
+                initial={{ y: -100, opacity: 0 }}
+                animate={{ y: 0, opacity: 1 }}
+                transition={{ duration: 0.5, ease: 'easeOut' }}
+                exit={{ y: -100, opacity: 0 }}
+            >
+                <div
+                    className="modal-content"
+                    onClick={(e) => e.stopPropagation()}
+                >
+                    <button className="close-btn" onClick={closeModal}>
                         <FaTimes />
                     </button>
                     {/* 로고 */}
-                    <div className='flex justify-center my-5'>
-                        <img src='/logo.png' width={230} alt='Logo' />
+                    <div className="my-5 flex justify-center">
+                        <img src="/logo.png" width={230} alt="Logo" />
                     </div>
 
                     {/* 이메일 회원가입 */}
-                    <Link href='/join' className='flex justify-center items-start rounded-md w-4/6 py-4 border-solid border border-gray-400 text-gray-500'> 
-                        <MdOutlineEmail className='text-md mt-0.5'/>
-                        <div className='pt-0.5 font-default ml-2'>이메일로 가입하기</div>
+                    <Link
+                        href="/join"
+                        className="flex w-4/6 items-start justify-center rounded-md border border-solid border-gray-400 py-4 text-gray-500"
+                    >
+                        <MdOutlineEmail className="mt-0.5 text-md" />
+                        <div className="font-default ml-2 pt-0.5">
+                            이메일로 가입하기
+                        </div>
                     </Link>
 
-                    <div className='flex items-center w-4/6 my-8'>
-                        <div className='h-0 w-1/3 border-t-2 mr-4 border-gray-300 border-solid'></div>
-                        <div className='text-gray-400 font-default text-xs w-1/2'>다른 방법으로 회원가입</div>
-                        <div className='h-0 w-1/3 border-t-2 ml-4 border-gray-300 border-solid'></div>
+                    <div className="my-8 flex w-4/6 items-center">
+                        <div className="mr-4 h-0 w-1/3 border-t-2 border-solid border-gray-300"></div>
+                        <div className="font-default w-1/2 text-xs text-gray-400">
+                            다른 방법으로 회원가입
+                        </div>
+                        <div className="ml-4 h-0 w-1/3 border-t-2 border-solid border-gray-300"></div>
                     </div>
 
                     {/* 소셜 로그인 */}
-                    <div className='flex gap-20 mb-7'>
-                        <img src='/kakao_login_round.png' width={60} className='cursor-pointer'/>
-                        <img src='/naver_login_round.png' width={60} className='cursor-pointer'/>
-                        <img src='/google_login_round.png' width={60} className='cursor-pointer'/>
+                    <div className="mb-7 flex gap-20">
+                        <img
+                            src="/kakao_login_round.png"
+                            width={60}
+                            className="cursor-pointer"
+                        />
+                        <img
+                            src="/naver_login_round.png"
+                            width={60}
+                            className="cursor-pointer"
+                        />
+                        <img
+                            src="/google_login_round.png"
+                            width={60}
+                            className="cursor-pointer"
+                        />
                     </div>
                 </div>
             </motion.div>
