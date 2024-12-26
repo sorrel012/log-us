ade06a4be24facb66958acddd9501680741d98b3
sorrel012 <wain1719@gmail.com>
2024-12-26 17:14:07 +0900

    fix: layout 타입 수정

--- src/app/[blogAddress]/(blog)/layout.tsx
+++ src/app/[blogAddress]/(blog)/layout.tsx
@@ -3,10 +3,10 @@ import BlogLayout from '@/components/blog/BlogLayout';
 export default function BlogRootLayout({
     children,
     modal,
-}: Readonly<{
+}: {
     children: React.ReactNode;
     modal: React.ReactNode;
-}>) {
+}) {
     return (
         <>
             {modal}
