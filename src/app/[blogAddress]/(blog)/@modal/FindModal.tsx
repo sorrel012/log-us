bfa7b1c280af8604526c17c71a5012371e25f260
sorrel012 <wain1719@gmail.com>
2024-12-26 17:23:20 +0900

    fix: emailjs 변수 타입 수정

--- src/app/[blogAddress]/(blog)/@modal/FindModal.tsx
+++ src/app/[blogAddress]/(blog)/@modal/FindModal.tsx
@@ -115,7 +115,7 @@ const FindModal: React.FC<FindModal> = ({ isOpen, closeModal, findType }) => {
 
                     try {
                         await emailjs.send(
-                            process.env.NEXT_PUBLIC_EMAIL_SERVICE_ID,
+                            process.env.NEXT_PUBLIC_EMAIL_SERVICE_ID!,
                             process.env.NEXT_PUBLIC_EMAIL_CODE_TEMPLATE_ID,
                             {
                                 to_email: email,
