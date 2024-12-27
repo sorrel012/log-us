1efb41924d0cb1d51f4d4f19e6018cede4775620
sorrel012 <wain1719@gmail.com>
2024-12-27 09:32:29 +0900

    fix: 타입 수정

--- src/components/blog/setting/FollowingGrid.tsx
+++ src/components/blog/setting/FollowingGrid.tsx
@@ -2,7 +2,11 @@ import FollowingCard, {
     FollowingsType,
 } from '@/components/blog/setting/FollowingCard';
 
-export default function FollowingGrid({ followings }: FollowingsType[] | null) {
+export default function FollowingGrid({
+    followings,
+}: {
+    followings: FollowingsType[];
+}) {
     return (
         <section className="flex flex-wrap gap-10">
             {followings.map((following: FollowingsType) => (
