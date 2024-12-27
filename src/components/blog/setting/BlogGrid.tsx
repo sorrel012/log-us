31214e4185bb07b439b8495986da744944f7ef2e
sorrel012 <wain1719@gmail.com>
2024-12-27 09:22:06 +0900

    fix: 타입 수정

--- src/components/blog/setting/BlogGrid.tsx
+++ src/components/blog/setting/BlogGrid.tsx
@@ -1,7 +1,7 @@
 import { Blog } from '@/components/UserGrid';
 import BlogCard from '@/components/blog/setting/BlogCard';
 
-export default function BlogGrid({ blogs }: Blog[] | null) {
+export default function BlogGrid({ blogs }: { blogs: Blog[] }) {
     return (
         <section className="flex flex-wrap gap-10">
             {blogs &&
