2ea1650dccd0b703e535e6987c821ce294fc837d
sorrel012 <wain1719@gmail.com>
2024-12-27 11:10:41 +0900

    fix: 타입 수정

--- src/app/setting/our-log/[blogAddress]/series/page.tsx
+++ src/app/setting/our-log/[blogAddress]/series/page.tsx
@@ -141,7 +141,7 @@ export default function SeriesManagePage() {
     const handleResetSeries = async (
         newSeriesId: number,
         mode: string,
-        body: {},
+        body: SaveFetchData,
     ) => {
         if (mode === 'DELETE') {
             await handleDelete(newSeriesId);
